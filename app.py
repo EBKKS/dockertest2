c13f573e34ad1a3b61e03134d4c3da6f35fe8afb
EBKKS <elias.bersch@kks.kbs.schule>
2024-12-10 13:56:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hee-hee!'
 
 if __name__ == '__main__':
     # Flask auf allen Schnittstellen lauschen lassen und Port 8080 verwenden
