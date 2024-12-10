c7fe7c38eb5394831520dfcdc71400c3393abf18
EBKKS <elias.bersch@kks.kbs.schule>
2024-12-10 13:53:58 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # Flask auf allen Schnittstellen lauschen lassen und Port 8080 verwenden
+    app.run(host='0.0.0.0', port=8080)
